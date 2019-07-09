3cb6f4013fc72a34eabb71042fdf0741959864c6
Chen-Dixi <cdxcqu2014@163.com>
2019-07-10 01:02:35 +0800

    保存预测图片，调度lr

--- train.py
+++ train.py
@@ -7,10 +7,13 @@ import torch.nn
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 from dixitool.pytorch.module import functional as F
+from torch.optim.lr_scheduler import StepLR
+from utils.visualize import Visualizer
 
 output_device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+visualizer = Visualizer()
 
-def train(net, criterion, train_dataloader, optimizer, epoch):
+def train(net, criterion, train_dataloader, optimizer,lr_scheduler, epoch):
     print("training")
     net.train()
     train_loss = 0.0
@@ -27,11 +30,13 @@ def train(net, criterion, train_dataloader, optimizer, epoch):
         optimizer.step()
 
         train_loss += loss.item()
+    lr_scheduler.step(epoch)
     print("epoch {epoch} loss:{loss}".format(epoch=epoch,loss=train_loss))
 
 best_iou = 0.0
 
-
+#每evaluate一段时间就看一下结果
+#画图
 def validation(net, criterion, val_dataloader, optimizer, mIoU_metric, epoch):
     print("evaluating")
     net.eval()
@@ -49,8 +54,12 @@ def validation(net, criterion, val_dataloader, optimizer, mIoU_metric, epoch):
         #每个batch更新一次metric
         mIoU_metric.update(outputs, targets)
 
-    miou = mIoU_metric.get()
+        if i == args.val.visualize_batch_index:
+            visualizer.visualize_predict('result',outputs, epoch)
 
+
+    miou = mIoU_metric.get()
+    print("epoch {epoch} miou:{miou}".format(epoch=epoch,miou=miou))
     if miou > best_miou:
         best_miou = miou
         F.save_model('checkpoints', 'mIoU[%.2f]' % best_miou, net)
@@ -67,7 +76,7 @@ def main(args):
     net = model.DeepLabV3Plus(backbone='mobilenet_v2', num_classes=args.data.num_classes)
     net = net.to_device_(args.train.num_gpus)
     ohem_ratio = 0.7
-    criterion = loss.CrossEntropyLossWithOHEM(ohem_ratio)
+    criterion = loss.CrossEntropyLossWithOHEM(ohem_ratio, ignore_index=255)
     mIoU = metric.MeanIoU(args.data.num_classes)
 
     
@@ -77,9 +86,10 @@ def main(args):
     val_dataset = CityscapesDataset(args.data.data_dir,split='val')
     val_dataloader = DataLoader(val_dataset, batch_size=args.data.batch_size)
 
-    optimizer = optim.SGD(net.parameters(),
+    optimizer = optim.SGD([{"params":net.parameters()}],
                           lr=args.train.lr,
                           weight_decay=args.train.weight_decay)
+    lr_scheduler = StepLR(optimizer,args.train.scheduler_step_size)
 
     opechs = args.train.epochs
 
@@ -92,7 +102,7 @@ def main(args):
     # print(outputs.size())#torch.Size([8, 19, 512, 512])
     for epoch in range(epochs):
         print("Epoch:{}/{}".format(epoch, epochs))
-        train(net, criterion, dataloader, optimizer, epoch)
+        train(net, criterion, dataloader, optimizer,lr_scheduler, epoch)
         if epoch % args.train.eval_interval == (args.train.eval_interval - 1)
             validation(net, criterion, val_dataloader, optimizer, mIoU, epoch)
 
