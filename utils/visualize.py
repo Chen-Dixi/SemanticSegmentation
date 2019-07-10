4fbceff983ac222a8522881bf3b7e45dc6842a35
Chen-Dixi <cdxcqu2014@163.com>
2019-07-10 14:04:10 +0800

    update

--- utils/visualize.py
+++ utils/visualize.py
@@ -8,7 +8,9 @@ class Visualizer(object):
     def __init__(self,name):
         self.name=name
 
-    def visualize_predict(self,root, pred,epoch):
+    def visualize_predict(self,root, pred,epoch ,mode='first'):
+        assert mode in ['first', 'all' , 'last']
+
         if not os.path.exists(root):
             os.mkdir(root)
         if pred.size(1) == 1:
@@ -16,14 +18,34 @@ class Visualizer(object):
             pred = pred > 0.5
         else:
             pred = torch.argmax(pred, dim=1)
-        for i in range(pred.size()[0]):
-            mask = pred[i].detach().cpu().numpy()
+
+        if mode == 'all':    
+            for i in range(pred.size()[0]):
+                mask = pred[i].detach().cpu().numpy()
+                mask = np.array(mask).astype(np.uint8)
+                self.save_predict(mask, os.path.join(root,"predict"+ str(i) +"epoch"+str(epoch)+".png"))
+                # segmap = decode_segmap(mask, dataset='cityscapes')
+                
+                # segmap = np.array(segmap * 255).astype(np.uint8)
+                # im = Image.fromarray(segmap)
+                # im.save( os.path.join(root,"predict"+ str(i) +"epoch"+str(epoch)+".png") )
+        elif mode == 'first':
+            mask = pred[0].detach().cpu().numpy()
             mask = np.array(mask).astype(np.uint8)
-            segmap = decode_segmap(mask, dataset='cityscapes')
-            
-            segmap = np.array(segmap * 255).astype(np.uint8)
-            im = Image.fromarray(segmap)
-            im.save( os.path.join(root,"predict"+ str(i) +"epoch"+str(epoch)+".png") )
+            self.save_predict(mask, os.path.join(root,"predict_epoch"+str(epoch)+".png") )
+        else:
+            mask = pred[-1].detach().cpu().numpy()
+            mask = np.array(mask).astype(np.uint8)
+            self.save_predict(mask, os.path.join(root,"predict_epoch"+str(epoch)+".png"))
+
+    def save_predict(self, mask, path):
+        
+        segmap = decode_segmap(mask, dataset='cityscapes')
+        
+        segmap = np.array(segmap * 255).astype(np.uint8)
+        im = Image.fromarray(segmap)
+        im.save( path)
+
             
 
 
