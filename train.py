1fdcde6d85223efb64be8fb85c0a6a464c7accf3
Chen-Dixi <cdxcqu2014@163.com>
2019-07-10 14:05:31 +0800

    保存checkpoint, 每次validation导出一张预测分割结果图片

--- train.py
+++ train.py
@@ -11,13 +11,13 @@ from torch.optim.lr_scheduler import StepLR
 from utils.visualize import Visualizer
 
 output_device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-visualizer = Visualizer()
+visualizer = Visualizer("asd")
 
-def train(net, criterion, train_dataloader, optimizer,lr_scheduler, epoch):
+def train(net, criterion, train_dataloader, optimizer, lr_scheduler, epoch):
     print("training")
     net.train()
     train_loss = 0.0
-    for i, sample in tqdm(enumerate(train_dataloader)):
+    for i, sample in enumerate(tqdm(train_dataloader)):
         images, targets = sample
         images = images.to(output_device)
         targets = targets.to(output_device)
@@ -25,51 +25,57 @@ def train(net, criterion, train_dataloader, optimizer,lr_scheduler, epoch):
 
         # 暂时不考虑调整learning rate
         outputs = net(images)
-        loss = criterion(outputs, targets)
+        loss = criterion(outputs, targets.long())
         loss.backward()
         optimizer.step()
 
         train_loss += loss.item()
+    
     lr_scheduler.step(epoch)
     print("epoch {epoch} loss:{loss}".format(epoch=epoch,loss=train_loss))
 
-best_iou = 0.0
+best_miou = 0.0
 
 #每evaluate一段时间就看一下结果
 #画图
-def validation(net, criterion, val_dataloader, optimizer, mIoU_metric, epoch):
+def validation(net, criterion, val_dataloader, optimizer, mIoU_metric, lr_scheduler, epoch):
     print("evaluating")
     net.eval()
     test_loss = 0.0
     mIoU_metric.reset()
-    best_miou = 0.0
+  
     for i, sample in tqdm(enumerate(val_dataloader)):
         images, targets = sample
         images = images.to(output_device)
         targets = targets.to(output_device)
         with torch.no_grad():
             outputs = net(images)
-        loss = criterion(outputs, targets)
+        loss = criterion(outputs, targets.long())
         test_loss += loss.item()
         #每个batch更新一次metric
         mIoU_metric.update(outputs, targets)
 
         if i == args.val.visualize_batch_index:
-            visualizer.visualize_predict('result',outputs, epoch)
-
+            # 模型预测结果导出图片，只导出第一个batch的第一张图片
+            visualizer.visualize_predict('result', outputs, epoch, mode='first')
 
     miou = mIoU_metric.get()
     print("epoch {epoch} miou:{miou}".format(epoch=epoch,miou=miou))
+    
+    #保存最优模型
     if miou > best_miou:
         best_miou = miou
         F.save_model('checkpoints', 'mIoU[%.2f]' % best_miou, net)
 
-
-
-
-
-
-
+    #保存存档点
+    #保存的内容， lr_scheduler, optimizer, net, epoch, best_mIoU
+    F.save_checkpoint({
+                'epoch': epoch + 1,
+                'state_dict': net.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'lr_scheduler': lr_scheduler.state_dict(),
+                'best_pred': best_miou,
+            }, False,'checkpoints' )
 
 def main(args):
     
@@ -91,7 +97,7 @@ def main(args):
                           weight_decay=args.train.weight_decay)
     lr_scheduler = StepLR(optimizer,args.train.scheduler_step_size)
 
-    opechs = args.train.epochs
+    epochs = args.train.epochs
 
     # it = iter(val_dataloader)
     # images,targets = it.next()
@@ -102,9 +108,9 @@ def main(args):
     # print(outputs.size())#torch.Size([8, 19, 512, 512])
     for epoch in range(epochs):
         print("Epoch:{}/{}".format(epoch, epochs))
-        train(net, criterion, dataloader, optimizer,lr_scheduler, epoch)
-        if epoch % args.train.eval_interval == (args.train.eval_interval - 1)
-            validation(net, criterion, val_dataloader, optimizer, mIoU, epoch)
+        train(net, criterion, dataloader, optimizer, lr_scheduler, epoch)
+        if epoch % args.train.eval_interval == (args.train.eval_interval - 1):
+            validation(net, criterion, val_dataloader, optimizer, mIoU, lr_scheduler, epoch)
 
 if __name__ == '__main__':
     main(args)
